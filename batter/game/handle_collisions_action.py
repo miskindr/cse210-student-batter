6766b5a017e58122ead4e615da619e53c774a5d1
austin eldredge <eld20009@byui.edu>
2021-11-04 14:21:19 -0600

    bugfix on collisions index

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -15,12 +15,14 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
+        i = 0
         paddle = cast["paddle"][0] # there's only one
         ball = cast["ball"][0] # there's only one
         bricks = cast["brick"]
         for brick in bricks:
             if ball.get_position().equals(brick.get_position()):
-                bricks.pop(brick.get_position())
+                bricks.pop(i)
+            i += 1
         for i in range(0,10):
             if paddle.get_position().get_x()+i == ball.get_position().get_x():
                 if paddle.get_position().get_y() == constants.MAX_Y-2:
