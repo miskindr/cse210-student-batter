29bf69965ded2d9f145b8870e900950990b9080b
JWilderman <kaodremora@outlook.com>
2021-11-04 17:39:42 -0400

    commenting control_actors_action

--- batter/game/control_actors_action.py
+++ batter/game/control_actors_action.py
@@ -27,5 +27,5 @@ class ControlActorsAction(Action):
             cast (dict): The game actors {key: tag, value: list}.
         """
         direction = self._input_service.get_direction()
-        paddle = cast["paddle"][0] # there's only one in the cast        Change when i figure out what need to go here
+        paddle = cast["paddle"][0] # there's only one in the cast
         paddle.set_velocity(direction)    
